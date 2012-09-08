47a1c33c48330764a087800314f87d49f791f66f
Ben Millwood <thebenmachine+git@gmail.com>
2012-09-08 15:29:02 +0100

    Fix Haddock parse error

--- src/Language/Haskell/TH/Instances/Lift.hs
+++ src/Language/Haskell/TH/Instances/Lift.hs
@@ -5,12 +5,12 @@
   Maintainer  :  Matt Morrow <mjm2002@gmail.com>
   Stability   :  experimental
   Portability :  portable (template-haskell)
--}
 
--- | This module is exported for backwards-compatibility purposes.
---   All it does is re-export the instances defined in 
---   "Language.Haskell.TH.Instances", from the th-orphans package.
+  This module is exported for backwards-compatibility purposes.
+  All it does is re-export the instances defined in 
+  "Language.Haskell.TH.Instances", from the th-orphans package.
+-}
 module Language.Haskell.TH.Instances.Lift
-  {-# DEPRECATED "Use the th-orphans package instead" #-} () where
+  {-# DEPRECATED "Use the th-orphans package instead." #-} () where
 
 import Language.Haskell.TH.Instances
