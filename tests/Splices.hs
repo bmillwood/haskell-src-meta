595d900583956f6861667361653d73acddbf7207
Jonas Duregard <jonas.duregard@chalmers.se>
2018-06-20 12:46:03 +0200

    Added a test case for a class+instance, and bumped version number

--- tests/Splices.hs
+++ tests/Splices.hs
@@ -3,7 +3,7 @@
 import Language.Haskell.Meta
 
 
------ Testing names:
+----- Testing names -----
 
 -- Test that the unit constructor works
 $(either error return $ parseDecs 
@@ -17,10 +17,16 @@ $(either error return $ parseDecs
       "pair :: (,) Int Int\npair = (,) 1 2")
 
 
+----- Testing classes and instances -----
+$(either error return $ parseDecs $ unlines
+   ["class MyClass a where mymethod :: a -> b -> (a,b)"
+   ,"instance MyClass Bool where mymethod a b = (a,b)"
+   ])
 
 -- Just to check that it works as intended
 main = do
   () <- unit
   [] <- return (nilp [])
   (1,2) <- return pair
+  (True,1) <- return $ mymethod True 1
   return ()
