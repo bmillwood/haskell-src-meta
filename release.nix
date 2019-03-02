b21964a06f551b718bb9b0aff248f86f122ef92d
Dan Burton <danburton.email@gmail.com>
2019-03-02 02:27:35 +0000

    Augment shell.nix with more tools

--- release.nix
+++ release.nix
@@ -1,12 +1,6 @@
 let
   pkgs = import <nixpkgs> { };
-  inherit (pkgs.haskellPackages) cabal-install ghcid;
-  inherit (builtins) concatLists;
-  inherit (pkgs.haskell.lib) addBuildTool overrideCabal failOnAllWarnings;
-  withCabalInstall = drv: addBuildTool drv cabal-install;
-  withGhcid = drv: addBuildTool drv ghcid;
-in
-  { package =
-      withGhcid (withCabalInstall
-        ( pkgs.haskellPackages.callPackage ./haskell-src-meta.nix { } ));
-  }
+  inherit (pkgs.haskellPackages) callPackage;
+in {
+  package = callPackage ./haskell-src-meta.nix { } ;
+}
--- shell.nix
+++ shell.nix
@@ -1,1 +1,12 @@
-(import ./release.nix).package.env
+let
+  # TODO: arg w/ default or whatever is idiomatic nix
+  pkgs = import <nixpkgs> { };
+  inherit (pkgs.haskell.lib) addBuildTools;
+  inherit (import ./release.nix) package;
+in
+  (addBuildTools package
+  # TODO: not pull lib versions of build tools if possible
+  ( with pkgs;
+    with pkgs.haskellPackages;
+    [cabal-install ghcid stylish-haskell shellcheck colordiff]
+  )).env
